ca5a1a37ff48ef0044e6b298478d712a7fa5fa19
Mia Altieri <miaaltieri@eduroam-169-233-181-231.ucsc.edu>
2019-02-04 12:59:51 -0800

    fixing mistakes

--- happy_script.sh
+++ happy_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # setting default args
-TRUTH_VCF=""
-FINAL_OUTPUT_VCF=""
-CONFIDENT_BED=""
+TRUTH_VCF="missing "
+FINAL_OUTPUT_VCF="missing"
+CONFIDENT_BED="missing"
 REF="missing"
-HAPPY_OUTPUT=""
+HAPPY_OUTPUT="missing"
+
 N_SHARDS=32
 REGION="none"
+MOUNT="/data/:/data/"
 
 # handeling arguments
 for i in "$@"
@@ -17,26 +19,23 @@ case $i in
   echo 
   "These are deep variant options:
     REQUIRED:
-      ref=<path_name> specifies reference, .fasta file
+      --ref=<path_name> specifies reference, .fasta file
+      --truth_vcf=<path_name> specifies the truth vcf directory
+      --final_output_vcf=<path_name> specifies the final output vcf directory 
+      --confident_bed=<path_name> specifies the confident bed directory 
+      --output=<path_name> specifies the output directory for hap.py 
 
     OPTIONAL:
-    specify output directories
-      --truth_vcf=<path_name> specifies the truth vcf directory
-          - defaults to 
-      --final_output_vcf=<path_name> specifies the final output vcf directory 
-          - defaults to 
-      --confident_bed=<path_name> specifies the confident bed directory 
-          - defaults to 
-      --happy_output=<path_name> specifies the output directory for hap.py 
-          - defaults to ./friday_outputs/vcf_output
       --region=<string> specifies region to run on
           - defaults to \"none\"
+      --mount=<string> specifies the mount point 
+          - defaults to /data/:/data/
       --threads=<int> specifies number of threads
           - defaults to 32 
     "
     exit 1
     ;;
-    ref=*)
+    --ref=*)
     REF="${i#*=}"
     shift
     ;;
@@ -60,6 +59,10 @@ case $i in
     REGION="${i#*=}"
     shift 
     ;;
+    --mount=*)
+    MOUNT="${i#*=}"
+    shift 
+    ;;
     --threads=*)
     N_SHARDS="${i#*=}"
     shift 
@@ -68,8 +71,9 @@ case $i in
     UNKNOWN="${i}"
     echo "unknown symbol ${UNKNOWN}"
     echo 
-    "usage: happy_script.sh [--help] ref=<path_name> [--truth_vcf=<path_name>] [--final_output_vcf=<path_name>]
-      [--confident_bed=<path_name>] [--happy_output=<path_name>] [--region=<string>] [--threads=<int>]"
+    "usage: happy_script.sh [--help] --ref=<path_name> [--truth_vcf=<path_name>] [--final_output_vcf=<path_name>]
+      [--confident_bed=<path_name>] [--happy_output=<path_name>] [--mount=<string>] [--region=<string>]
+      [--threads=<int>]"
     exit 1
     ;;
 esac
@@ -80,17 +84,21 @@ if [ "$REF" = "missing" ]
 then
   echo "ref missing"
   echo 
-  "usage: happy_script.sh [--help] ref=<path_name> [--truth_vcf=<path_name>] [--final_output_vcf=<path_name>]
-    [--confident_bed=<path_name>] [--happy_output=<path_name>] [--region=<string>] [--threads=<int>]"
+  "usage: happy_script.sh [--help] --ref=<path_name> [--truth_vcf=<path_name>] [--final_output_vcf=<path_name>]
+    [--confident_bed=<path_name>] [--happy_output=<path_name>] [--mount=<string>] [--region=<string>]
+    [--threads=<int>]"
   exit 1
 fi
+
+
+CHR_NAME="$(basename "${FINAL_OUTPUT_VCF}" .vcf.gz)"
 
 # hap.py code 
 sudo docker pull pkrusche/hap.py
 mkdir -p ${HAPPY_OUTPUT}
 
 if [ "$REGION" = "none" ]; then
-  time sudo docker run -it -v  /data/:/data/ \
+  time sudo docker run -it -v ${MOUNT} \
     pkrusche/hap.py /opt/hap.py/bin/hap.py \
     ${TRUTH_VCF} \
     "${FINAL_OUTPUT_VCF}" \
@@ -98,15 +106,15 @@ if [ "$REGION" = "none" ]; then
     -r "${REF}" \
     -o "${HAPPY_OUTPUT}/chr20" \
     --engine=vcfeval \
-    --threads=${N_SHARDS} \
+    --threads=${N_SHARDS}
 else 
-  time sudo docker run -it -v  /data/:/data/ \
+  time sudo docker run -it -v ${MOUNT} \
     pkrusche/hap.py /opt/hap.py/bin/hap.py \
     ${TRUTH_VCF} \
     "${FINAL_OUTPUT_VCF}" \
     -f "${CONFIDENT_BED}" \
     -r "${REF}" \
-    -o "${HAPPY_OUTPUT}/chr20" \
+    -o "${HAPPY_OUTPUT}/${CHR_NAME}" \
     --engine=vcfeval \
     --threads=${N_SHARDS} \
     -l "${REGION}"
