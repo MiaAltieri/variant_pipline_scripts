d8c9ddbce63f75ba9394001c34079b366c823f0c
Mia Altieri <miaaltieri@eduroam-169-233-181-231.ucsc.edu>
2019-02-04 13:04:09 -0800

    cleaning

--- happy_script.sh
+++ happy_script.sh
@@ -21,7 +21,7 @@ case $i in
     REQUIRED:
       --ref=<path_name> specifies reference, .fasta file
       --truth_vcf=<path_name> specifies the truth vcf directory
-      --final_output_vcf=<path_name> specifies the final output vcf directory 
+      --final_output_vcf=<path_name> specifies the final vcf file 
       --confident_bed=<path_name> specifies the confident bed directory 
       --output=<path_name> specifies the output directory for hap.py 
 
@@ -80,10 +80,10 @@ esac
 done
 
 # checking for required arguments 
-if [ "$REF" = "missing" ]
+if [ "$REF" = "missing" ]  || [ "$TRUTH_VCF" = "missing" ] || [ "$FINAL_OUTPUT_VCF" = "missing" ] || [ "$CONFIDENT_BED" = "missing" ] || [ "$HAPPY_OUTPUT" = "missing" ]
 then
-  echo "ref missing"
-  echo 
+  echo "ref, truth vcf, final output, confident_bed, or output missing"
+  echo
   "usage: happy_script.sh [--help] --ref=<path_name> [--truth_vcf=<path_name>] [--final_output_vcf=<path_name>]
     [--confident_bed=<path_name>] [--happy_output=<path_name>] [--mount=<string>] [--region=<string>]
     [--threads=<int>]"
